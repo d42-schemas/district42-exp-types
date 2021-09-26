5b750e2b17e3d734a7114342a9aa59c436e3aa06
Nikita Tsvetkov <nikitanovosibirsk@yandex.com>
2021-09-26 18:56:49 +0700

    add substitutor tests

--- district42_exp_types/multidict/_multidict_substitutor.py
+++ district42_exp_types/multidict/_multidict_substitutor.py
@@ -1,14 +1,14 @@
 from copy import deepcopy
-from typing import Any, Mapping
+from typing import Any, List, Mapping
 
 from district42 import GenericSchema
 from multidict import MultiDict
 from niltype import Nil, Nilable
 from revolt import Substitutor, SubstitutorValidator
-from revolt.errors import SubstitutionError, make_substitution_error
+from revolt.errors import make_substitution_error, SubstitutionError
 from th import PathHolder
 from valera import ValidationResult
-from valera.errors import ExtraKeyValidationError, TypeValidationError
+from valera.errors import ExtraKeyValidationError, TypeValidationError, ValidationError
 
 from ._multidict_schema import MultiDictSchema
 
@@ -18,6 +18,13 @@ __all__ = ("MultiDictSubstitutor", "MultiDictSubstitutorValidator",)
 class MultiDictSubstitutor(Substitutor, extend=True):
     def visit_multi_dict(self, schema: MultiDictSchema, *,
                          value: Any = Nil, **kwargs: Any) -> MultiDictSchema:
+        if not isinstance(value, MultiDict):
+            try:
+                value = MultiDict(value)
+            except Exception as e:
+                message = f"Can't substitute to MultiDict: {e}"
+                raise SubstitutionError(message)
+
         result = schema.__accept__(self._validator, value=value)
         if result.has_errors():
             raise make_substitution_error(result, self._formatter)
@@ -40,6 +47,17 @@ class MultiDictSubstitutor(Substitutor, extend=True):
 
 
 class MultiDictSubstitutorValidator(SubstitutorValidator, extend=True):
+    def __validate_candidates(self, value: GenericSchema, path: PathHolder,
+                              candidates: List[Any], **kwargs: Any) -> List[ValidationError]:
+        all_errors = []
+        for candidate in candidates:
+            res = value.__accept__(self, value=candidate, path=path, **kwargs)
+            if not res.has_errors():
+                return []
+            all_errors.append(res.get_errors())
+        all_errors.sort(key=len)
+        return all_errors[0]
+
     def visit_multi_dict(self, schema: MultiDictSchema, *,
                          value: Any = Nil, path: Nilable[PathHolder] = Nil,
                          **kwargs: Any) -> ValidationResult:
@@ -53,15 +71,27 @@ class MultiDictSubstitutorValidator(SubstitutorValidator, extend=True):
         if schema.props.keys is Nil:
             return result
 
-        for key, val in schema.props.keys.items():
-            if key in value:
-                nested_path = deepcopy(path)[key]
+        for key in set(schema.props.keys):
+            if key not in value:
+                continue
+
+            nested_path = deepcopy(path)[key]
+            if isinstance(value, MultiDict):
+                candidates = value.getall(key)
+                for val in schema.props.keys.getall(key):
+                    errors = self.__validate_candidates(val, nested_path, candidates, **kwargs)
+                    result.add_errors(errors)
+            else:
+                val = schema.props.keys.getone(key)
                 res = val.__accept__(self, value=value[key], path=nested_path, **kwargs)
                 result.add_errors(res.get_errors())
 
-        if len(schema.props.keys) != len(value):
-            for key, val in value.items():
-                if key not in schema.props.keys:
+        for key in set(value):
+            if key not in schema.props.keys:
+                result.add_error(ExtraKeyValidationError(path, value, key))
+            else:
+                vals = value.getall(key) if isinstance(value, MultiDict) else [value.get(key)]
+                if len(vals) > len(schema.props.keys.getall(key)):
                     result.add_error(ExtraKeyValidationError(path, value, key))
 
         return result
