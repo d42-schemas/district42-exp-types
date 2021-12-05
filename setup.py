e36e43da62f32d58d4b14c65afa16856dc8849e1
Nikita Tsvetkov <nikitanovosibirsk@yandex.com>
2021-12-05 15:23:31 +0300

    bump version → 0.8.0

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ def find_dev_required():
 
 setup(
     name="district42-exp-types",
-    version="0.7.1",
+    version="0.8.0",
     description="district42 experimental types",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
